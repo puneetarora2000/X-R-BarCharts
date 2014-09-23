633bb988e8f410214dfc1454a2ddb61c7fb4da7d
puneetarora2000 <puneetarora2000@gmail.com>
2014-09-23 22:19:28 +0530

    Adding Bar Graphs

--- src/mythresholding/MainDecomposition.java
+++ src/mythresholding/MainDecomposition.java
@@ -9,6 +9,23 @@ import org.jfree.data.category.CategoryDataset;
 import java.io.File;
 import java.io.IOException;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+ 
+
 /*
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
@@ -50,8 +67,115 @@ public class MainDecomposition {
     return limits;
  
  }
+ 
+ 
+ 
+ private CategoryDataset createDataset() {
+        
+        // row keys...
+        final String series1 = "First Metric";
+        final String series2 = "Second Metric";
+        final String series3 = "Third Metric";
+
+        // column keys...
+        final String category1 = "Metric 1";
+        final String category2 = "Metric 2";
+        final String category3 = "Metric 3";
+        final String category4 = "Metric 4";
+        final String category5 = "Metric 5";
+
+        // create the dataset...
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        dataset.addValue(1.0, series1, category1);
+        dataset.addValue(4.0, series1, category2);
+        dataset.addValue(3.0, series1, category3);
+        dataset.addValue(5.0, series1, category4);
+        dataset.addValue(5.0, series1, category5);
+
+        dataset.addValue(5.0, series2, category1);
+        dataset.addValue(7.0, series2, category2);
+        dataset.addValue(6.0, series2, category3);
+        dataset.addValue(8.0, series2, category4);
+        dataset.addValue(4.0, series2, category5);
+
+        dataset.addValue(4.0, series3, category1);
+        dataset.addValue(3.0, series3, category2);
+        dataset.addValue(2.0, series3, category3);
+        dataset.addValue(3.0, series3, category4);
+        dataset.addValue(6.0, series3, category5);
+        
+        return dataset;
+        
+    }
     
+ 
+ private JFreeChart createChart(final CategoryDataset dataset) {
+        
+        // create the chart...
+        final JFreeChart chart = ChartFactory.createBarChart(
+            "Threshold Limits",         // chart title
+            "Metrics",               // domain axis label
+            "Value",                  // range axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL, // orientation
+            true,                     // include legend
+            true,                     // tooltips?
+            false                     // URLs?
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+
+        // set the background color for the chart...
+        chart.setBackgroundPaint(Color.white);
+
+        // get a reference to the plot for further customisation...
+        final CategoryPlot plot = chart.getCategoryPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+
+        // set the range axis to display integers only...
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+        // disable bar outlines...
+        final BarRenderer renderer = (BarRenderer) plot.getRenderer();
+        renderer.setDrawBarOutline(false);
+        
+        // set up gradient paints for series...
+        final GradientPaint gp0 = new GradientPaint(
+            0.0f, 0.0f, Color.blue, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        final GradientPaint gp1 = new GradientPaint(
+            0.0f, 0.0f, Color.green, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        final GradientPaint gp2 = new GradientPaint(
+            0.0f, 0.0f, Color.red, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        renderer.setSeriesPaint(0, gp0);
+        renderer.setSeriesPaint(1, gp1);
+        renderer.setSeriesPaint(2, gp2);
+
+        final CategoryAxis domainAxis = plot.getDomainAxis();
+        domainAxis.setCategoryLabelPositions(
+            CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0)
+        );
+        // OPTIONAL CUSTOMISATION COMPLETED.
+        
+        return chart;
+        
+    }
+ 
+ 
+ 
     
+  
+ 
+ 
     
     
     
@@ -66,38 +190,17 @@ public class MainDecomposition {
     
     double[] limits1 = FindLimits("StabilityMetric M1", M1);
      
+    
+        
+    
+    
+    
       
     }
     
     
     
-    private static  CategoryDataset  createDataset(double limits1[],double limits2[],double limits3[],double limits4[],double limits5[]) {
-
-    // row keys...
-    String series1 = "StabilityMetric1";
-    String series2 = "StabilityMetric2";
-    String series3 = "StabilityMetric3";
-    String series4 = "StabilityMetric4";
-    String series5 = "StabilityMetric5";
-
-    // column keys...
-    String category1 = "StabilityMetric 1";
-    String category2 = "StabilityMetric 2";
-    String category3 = "StabilityMetric 3";
-    String category4 = "StabilityMetric 4";
-    String category5 = "StabilityMetric 5";
-    
      
-
-    // create the dataset...
-         
-        
-        
-          
-    
-    return null;
-
-}
 
      
      
